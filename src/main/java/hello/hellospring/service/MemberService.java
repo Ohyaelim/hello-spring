659f79ce8621f5b735fb31f124e136ccd73595a5
ohyaelim <npr05080@gmail.com>
2021-01-12 14:30:52 +0900

    direct spring bean

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -10,13 +10,11 @@ import java.util.List;
 import java.util.Optional;
 
 //test 할땐 command+shift+t
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     //직접 new 로 생성하는게 아니라 외부에서 넣어주도록 바꾸는것
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

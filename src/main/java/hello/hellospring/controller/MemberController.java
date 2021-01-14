28fc445a3cbe1166cbb0ae5cee0f408287c4cc09
ohyaelim <npr05080@gmail.com>
2021-01-14 13:52:31 +0900

    Membership Web Features - inquiry

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.awt.*;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,5 +32,14 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        //memberService.findMembers();
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
 }
